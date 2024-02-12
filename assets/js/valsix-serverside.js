4edb6175907996b63cf9990fa3b69bddab0100c6
Septian Eko <123433177+SepEko@users.noreply.github.com>
2024-02-12 16:01:02 +0700

    exclude search column no

--- assets/js/valsix-serverside.js
+++ assets/js/valsix-serverside.js
@@ -521,25 +521,35 @@ var ajaxserverselectsingle = function() {
 
                         if(title !== "Pilih")
                         {
-                            if ($(api.column(colIdx).header()).index() >= 0) {
-                               $(cell).html('<input type="text" style="color: #000000;" placeholder="' + title + '"/>');
+                            if(title  !== "No")
+                            {
+
+                                if ($(api.column(colIdx).header()).index() >= 0) {
+                                   $(cell).html('<input type="text" style="color: #000000;" placeholder="' + title + '"/>');
+                                }
+                                // On every keypress in this input
+                                $('input', $('.filters th').eq($(api.column(colIdx).header()).index()) )
+                                    .off('keyup change')
+                                    .on('keyup change', function (e) {
+                                        e.stopPropagation();
+                                        // Get the search value
+                                        $(this).attr('title', $(this).val());
+                                        var regexr = '({search})'; //$(this).parents('th').find('select').val();
+                                        var cursorPosition = this.selectionStart;
+                                        // Search the column for that value
+                                        api
+                                        .column(colIdx)
+                                        .search((this.value != "") ? regexr.replace('{search}', '((('+this.value+')))') : "", this.value != "", this.value == "")
+                                        .draw();
+                                    $(this).focus()[0].setSelectionRange(cursorPosition, cursorPosition);
+                                });
+
                             }
-                            // On every keypress in this input
-                            $('input', $('.filters th').eq($(api.column(colIdx).header()).index()) )
-                                .off('keyup change')
-                                .on('keyup change', function (e) {
-                                    e.stopPropagation();
-                                    // Get the search value
-                                    $(this).attr('title', $(this).val());
-                                    var regexr = '({search})'; //$(this).parents('th').find('select').val();
-                                    var cursorPosition = this.selectionStart;
-                                    // Search the column for that value
-                                    api
-                                    .column(colIdx)
-                                    .search((this.value != "") ? regexr.replace('{search}', '((('+this.value+')))') : "", this.value != "", this.value == "")
-                                    .draw();
-                                $(this).focus()[0].setSelectionRange(cursorPosition, cursorPosition);
-                            });
+                            else
+                            {
+                                 $(cell).html('');
+                            }
+                           
                         }
                         else
                         {
