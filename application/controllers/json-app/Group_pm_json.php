892821f40fe3189e2934820a1c1bedb494b1661f
Septian Eko <123433177+SepEko@users.noreply.github.com>
2024-06-14 14:33:38 +0700

    session filter unit mesin group pm

--- application/controllers/json-app/Group_pm_json.php
+++ application/controllers/json-app/Group_pm_json.php
@@ -23,6 +23,7 @@ class Group_pm_json extends CI_Controller
 		$this->personaluserlogin= $this->session->userdata("personaluserlogin");
 		$this->appusergroupid= $this->session->userdata("appusergroupid");
 		$this->appblokunitid= $this->session->userdata("appblokunitid");
+		$this->appunitmesinid= $this->session->userdata("appunitmesinid");
 
 		$this->configtitle= $this->config->config["configtitle"];
 		// print_r($this->configtitle);exit;
@@ -69,7 +70,13 @@ class Group_pm_json extends CI_Controller
 
 		if(!empty($this->appblokunitid))
 		{
-			$statement.= " AND D.BLOK_UNIT_ID = ".$this->appblokunitid;
+			$statement.= " AND C.BLOK_UNIT_ID = ".$this->appblokunitid;
+		}
+
+
+		if(!empty($this->appunitmesinid))
+		{
+			$statement.= " AND D.UNIT_MESIN_ID = ".$this->appunitmesinid;
 		}
 
 		$sOrder = " ORDER BY A.KODE_UNIT,A.GROUP_PM ASC ";
