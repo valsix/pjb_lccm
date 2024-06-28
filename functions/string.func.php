47798fa193e037a236fa2a90c9d747f236e3407d
Septian Eko <123433177+SepEko@users.noreply.github.com>
2024-06-28 18:47:10 +0700

    function global rate kalkulasi

--- functions/string.func.php
+++ functions/string.func.php
@@ -1694,5 +1694,60 @@ function decimalHours($time,$mode)
 	return $hasil;
 }
 
+function kalkulasi($reqTahunAwal,$reqTahunAkhir,$product=1)
+{
+	$product=number_format((float)$product, 8, '.', '');
+	$jangkawaktu= $reqTahunAkhir-$reqTahunAwal;
+
+	$nper = $jangkawaktu;
+	$pmt = 0;
+	$pv = -1;
+	$fv = $product;
+	$type = 0;
+	$guess = 0.1;
+	$rate=RATE($nper, $pmt, $pv, $fv, $guess);
+	$rate=round($rate, 4);
+	$rate=$rate*100;
+	$total=$rate;
+	
+	return $total;
+}
+
+
+function RATE($nper, $pmt, $pv, $fv = 0.0, $type = 0, $guess = 0.1) {
+	define('FINANCIAL_MAX_ITERATIONS', 128);
+	define('FINANCIAL_PRECISION', 1.0e-08);
+
+	$rate = $guess;
+	if (abs($rate) < FINANCIAL_PRECISION) {
+		$y = $pv * (1 + $nper * $rate) + $pmt * (1 + $rate * $type) * $nper + $fv;
+	} else {
+		$f = exp($nper * log(1 + $rate));
+		$y = $pv * $f + $pmt * (1 / $rate + $type) * ($f - 1) + $fv;
+	}
+	$y0 = $pv + $pmt * $nper + $fv;
+	$y1 = $pv * $f + $pmt * (1 / $rate + $type) * ($f - 1) + $fv;
+
+	$i  = $x0 = 0.0;
+	$x1 = $rate;
+	while ((abs($y0 - $y1) > FINANCIAL_PRECISION) && ($i < FINANCIAL_MAX_ITERATIONS)) {
+		$rate = ($y1 * $x0 - $y0 * $x1) / ($y1 - $y0);
+		$x0 = $x1;
+		$x1 = $rate;
+
+		if (abs($rate) < FINANCIAL_PRECISION) {
+			$y = $pv * (1 + $nper * $rate) + $pmt * (1 + $rate * $type) * $nper + $fv;
+		} else {
+			$f = exp($nper * log(1 + $rate));
+			$y = $pv * $f + $pmt * (1 / $rate + $type) * ($f - 1) + $fv;
+		}
+
+		$y0 = $y1;
+		$y1 = $y;
+		++$i;
+	}
+	return $rate;
+}  
+
 
 ?>
